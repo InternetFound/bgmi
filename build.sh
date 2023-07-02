4db9d5561b220b7e83602b3e1672a695a04202c0
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-07-02 18:21:41 +0530

    B | R

--- build.sh
+++ build.sh
@@ -2,15 +2,10 @@
 . build/envsetup.sh
 lunch superior_lavender-userdebug
 
-build_gapps=1
-
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-
-exp_gapps() {
 export BUILD_WITH_GAPPS=true
-}
 
 compile_plox () {
 m bacon -j10
