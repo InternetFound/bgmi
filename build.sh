eec1e9b2c43eac59707987fa2ee646f03d3ed1c6
Prashant-1695 <prashant33968@gmail.com>
2023-07-02 10:26:58 +0530

    B | V

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch superior_lavender-userdebug
+
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
-# export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
 
+exp_gapps() {
+export BUILD_WITH_GAPPS=true
+}
+
 compile_plox () {
-mka bacon -j10
+m bacon -j10
 }
