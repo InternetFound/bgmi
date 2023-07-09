e16d4559519559f37969e6df5e2b6ea0d4720061
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-07-09 10:59:14 +0530

    B | R

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ lunch superior_lavender-userdebug
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-# export BUILD_WITH_GAPPS=true
+export BUILD_WITH_GAPPS=true
 export TARGET_INCLUDE_PIXEL_CHARGER=true
 
 compile_plox () {
