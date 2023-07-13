9119f8ff7b6eff05df8d82d1eab9ae9ad1997a7e
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-07-13 18:18:23 +0530

    both variant [skip ci]

--- build.sh
+++ build.sh
@@ -2,11 +2,17 @@
 . build/envsetup.sh
 lunch superior_lavender-userdebug
 
+build_gapps=1
+
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_WITH_GAPPS=true
 export TARGET_INCLUDE_PIXEL_CHARGER=true
+
+exp_gapps() { 
+export BUILD_WITH_GAPPS=true
+}
 
 compile_plox () {
 m bacon -j10
