c584420e5ed3a7d6ac26bbf766ee2ca7302477ba
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-07-13 09:16:26 +0530

    B | R

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 . build/envsetup.sh
 lunch lineage_lavender-userdebug
 
+build_gapps=1
+
 # export variable here
 export TZ=Asia/Kolkata
 # export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
+
+exp_gapps() {
 export WITH_GAPPS=true
 export GAPPS_BUILD_TYPE=2
-#export BUILD_USERNAME=Prashant
-#export BUILD_HOSTNAME=ArchLinux
+}
 
 compile_plox () {
 make bacon -j10
