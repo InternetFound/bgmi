c71358df307c5f855d8db1a420706ccc30702548
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-07-16 12:45:53 +0530

    B | V

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ lunch lineage_lavender-userdebug
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 # export TARGET_INCLUDE_PIXEL_CHARGER=true
-export WITH_GAPPS=false
+# export WITH_GAPPS=false
 
 # exp_gapps() { 
 # export WITH_GAPPS=true
