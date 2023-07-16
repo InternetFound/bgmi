9b1526aee79ae12e22890394ec4a94e17d7f9fc4
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-07-16 12:04:45 +0530

    B | V

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 . build/envsetup.sh
 lunch lineage_lavender-userdebug
 
-build_gapps=1
+# build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TARGET_INCLUDE_PIXEL_CHARGER=true
+# export TARGET_INCLUDE_PIXEL_CHARGER=true
 export WITH_GAPPS=false
 
-exp_gapps() { 
-export WITH_GAPPS=true
-}
+# exp_gapps() { 
+# export WITH_GAPPS=true
+# }
 
 compile_plox () {
-m bacon -j10
+mka bacon -j10
 }
