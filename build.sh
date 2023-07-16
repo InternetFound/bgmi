67788cba273315702147561150384a2c1a03e54a
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-07-16 23:42:41 +0530

    B | V

--- build.sh
+++ build.sh
@@ -2,17 +2,10 @@
 . build/envsetup.sh
 lunch lineage_lavender-userdebug
 
-# build_gapps=1
-
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-# export TARGET_INCLUDE_PIXEL_CHARGER=true
-# export WITH_GAPPS=false
-
-# exp_gapps() { 
-# export WITH_GAPPS=true
-# }
+export TARGET_INCLUDE_PIXEL_CHARGER=true
 
 compile_plox () {
 mka bacon -j10
