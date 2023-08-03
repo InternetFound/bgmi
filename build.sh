9a8de0a5f0ba286873f94419e404682cbd3923eb
Prashant-1695 <prashant33968@gmail.com>
2023-08-03 20:12:21 +0530

    B | V

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Normal build steps
 . build/envsetup.sh
-lunch legion_lavender-userdebug
+lunch superior_lavender-userdebug
 
 build_gapps=1
 
@@ -9,14 +9,12 @@ export TZ=Asia/Kolkata
 # export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
-export LEGION_BUILD_TYPE=K4.19
-
-# necessary for building gapps rom on retro dynamic
-export WITH_GMS=true
 
 exp_gapps () {
 export LEGION_GAPPS=true
 export WITH_GAPPS=true
+export WITH_GMS=true
+export BUILD_WITH_GAPPS=true
 }
 
 compile_plox () {
