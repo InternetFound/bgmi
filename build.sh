af2deeaf58d6d023056b7aed4de785b34626f74a
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-08-11 05:33:35 +0530

    B | R

--- build.sh
+++ build.sh
@@ -2,12 +2,15 @@
 . build/envsetup.sh
 lunch superior_lavender-userdebug
 
-build_gapps=1
+build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TARGET_INCLUDE_PIXEL_CHARGER=true
+export BUILD_WITH_GAPPS=true
+export WITH_GAPPS=true
+export WITH_GMS=true
 
 exp_gapps() { 
 export BUILD_WITH_GAPPS=true
