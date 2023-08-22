e2c5c3b89b48438f917111ae1df36a86ef88691b
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-08-22 14:34:53 +0530

    B | R

--- build.sh
+++ build.sh
@@ -1,11 +1,23 @@
 # Normal build steps
 . build/envsetup.sh
-lunch aosp_lavender-userdebug
+lunch evolution_lavender-userdebug
+
+build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
+# export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
+export RELAX_USES_LIBRARY_CHECK=true
+export WITH_GMS=true
+
+exp_gapps () {
+export LEGION_GAPPS=true
+export WITH_GAPPS=true
+export WITH_GMS=true
+export BUILD_WITH_GAPPS=true
+}
 
 compile_plox () {
-mka bacon -j10
+make bacon -j10
 }
